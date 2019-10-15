2685e9355ed5cdc388e80fe8214baa0ba3c34e9c
Alberto <info@androiddesdecero.com>
2019-10-15 06:45:20 +0200

    2

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const graphqlHTTP = require('express-graphql')
 
 const app = express()
 
+app.use('/graphql', graphqlHTTP({
+
+}))
+
 app.listen(3131, ()=>{
     console.log('Escuchando del puerto: 3131')
 })
