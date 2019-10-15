c6a1186d26c55a21ba225766bd391a45e742b6e9
Alberto <info@androiddesdecero.com>
2019-10-15 06:54:09 +0200

    2

--- schema/schema.js
+++ schema/schema.js
@@ -1,1 +1,14 @@
 const graphql = require('graphql')
+
+const {GraphQLObjectType, GraphQLString} = graphql
+
+const CourseType = new GraphQLObjectType({
+    name: 'Course',
+    fields: ()=>({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        language: {type: GraphQLString},
+        date: {type: GraphQLString}
+    })
+
+})
