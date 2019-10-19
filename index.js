2c59f9c28606ed665ca69151c93c1168b7a387bc
Alberto <info@androiddesdecero.com>
2019-10-19 19:09:41 +0200

    3

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/coursedb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true})
+  .then(()=> console.log('Conectado a MongoDB correctamente'))
+  .catch(err => console.log('No se ha Conectado a MongoDB correctamente'))
 
 const app = express()
 
